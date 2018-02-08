ae2889fa4081d93aa843c38f61a6108913214b91
gminardi <gioele.minardi@nais-solutions.it>
2018-02-08 17:47:17 +0100

    Shallow Copy vs Deep Copy

---
+++ CopyCtor/Integer.h
@@ -0,0 +1,26 @@
+//
+// Created by gminardi on 08/02/18.
+//
+
+#ifndef COPYCTOR_INTEGER_H
+#define COPYCTOR_INTEGER_H
+
+
+class Integer {
+private:
+    int *m_pInt;
+public:
+    Integer();
+
+    Integer(int value);
+
+    int GetValue() const;
+
+    void SetValue(int value);
+
+    virtual ~Integer();
+
+};
+
+
+#endif //COPYCTOR_INTEGER_H
