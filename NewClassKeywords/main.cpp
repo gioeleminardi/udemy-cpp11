1b4f95ca7d5e9a8f450af44059f63675e0ca196d
gminardi <gioele.minardi@nais-solutions.it>
2018-02-08 18:27:19 +0100

    Delete keyword

--- NewClassKeywords/main.cpp
+++ NewClassKeywords/main.cpp
@@ -4,18 +4,29 @@ class Integer {
     int m_Value{0};
 public:
     Integer() = default;
+
 //    Integer() {
 //        m_Value = 0;
 //    }
     Integer(int value) {
         m_Value = value;
     }
-    // Copy Ctor REDUNDANT
-    Integer(const Integer &) = default;
+
+    // With delete compiler will not generate default copy ctor
+    Integer(const Integer &) = delete;
+
+    void SetValue(int value) {
+        m_Value = value;
+    }
+
+    void SetValue(float) = delete;
 };
 
 int main() {
     Integer i1;
     Integer i2(3);
+    i1.SetValue(42);
+    // Deleted function
+//    i1.SetValue(3f);
     return 0;
 }
