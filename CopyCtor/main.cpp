09590d3bcf2b04433b548db80a11b648246e482e
gminardi <gioele.minardi@nais-solutions.it>
2018-02-08 19:08:29 +0100

    std::move

--- CopyCtor/main.cpp
+++ CopyCtor/main.cpp
@@ -9,26 +9,33 @@
 //    return Integer(x + y);
 //}
 
-Integer Add(const Integer &a, const Integer &b) {
-    Integer temp;
-    temp.SetValue(a.GetValue() + b.GetValue());
-    return temp;
+//Integer Add(const Integer &a, const Integer &b) {
+//    Integer temp;
+//    temp.SetValue(a.GetValue() + b.GetValue());
+//    return temp;
+//}
+
+void Process(Integer val) {
+
 }
 
 int main() {
-// //    int *p1 = new int(5);
-// //    // Shallow Copy
-// //    int *p2 = p1;
-// //    // Deep Copy
-// //    int *p3 = new int(*p1);
-//
-//    Integer i(5);
-//    // Copy Constructor -> Shallow Copy by Default
-//    Integer i2(i);
-//    // Shallow Copy by default
-// //    i = i2;
-//    std::cout << i.GetValue() << std::endl;
-    Integer a(1), b(3);
-    a.SetValue(Add(a, b).GetValue());
+// // //    int *p1 = new int(5);
+// // //    // Shallow Copy
+// // //    int *p2 = p1;
+// // //    // Deep Copy
+// // //    int *p3 = new int(*p1);
+// //
+// //    Integer i(5);
+// //    // Copy Constructor -> Shallow Copy by Default
+// //    Integer i2(i);
+// //    // Shallow Copy by default
+// // //    i = i2;
+// //    std::cout << i.GetValue() << std::endl;
+//    Integer a(1), b(3);
+//    a.SetValue(Add(a, b).GetValue());
+    Integer a(1), c(2);
+    auto b(std::move(a));
+    Process(std::move(c));
     return 0;
 }
