d5bde85a7f13b15ae927c9e7bc26c53947d0f8be
gioeleminardi <gioelem3@gmail.com>
2018-02-08 22:58:10 +0100

    Smart Ptrs C++11

--- SmartPointer/main.cpp
+++ SmartPointer/main.cpp
@@ -14,17 +14,45 @@ public:
         return m_p;
     }
 
-    Integer &operator*(){
+    Integer &operator*() {
         return *m_p;
     }
 };
 
-void CreateInteger() {
-    IntPtr p = new Integer;
-    p->SetValue(3);
-    (*p).SetValue(4);
+
+
+
+////Smart Shared Ptr C++11
+void Process(std::shared_ptr<Integer> ptr) {
+    std::cout << ptr->GetValue() << std::endl;
+}
+
+void CreateInteger(){
+    std::shared_ptr<Integer> p(new Integer);
+    (*p).SetValue(3);
+    Process(p);
     std::cout << p->GetValue() << std::endl;
 }
+
+////Smart Unique Ptr C++11
+//void Process(std::unique_ptr<Integer> ptr) {
+//    std::cout << ptr->GetValue() << std::endl;
+//}
+//
+//void CreateInteger(){
+//    std::unique_ptr<Integer> p(new Integer);
+//    (*p).SetValue(3);
+//    std::cout << p->GetValue() << std::endl;
+//    Process(std::move(p));
+//}
+
+// Smart Ptr Basic
+//void CreateInteger() {
+//    IntPtr p = new Integer;
+//    p->SetValue(3);
+//    (*p).SetValue(4);
+//    std::cout << p->GetValue() << std::endl;
+//}
 
 int main() {
     CreateInteger();
