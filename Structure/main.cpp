8b03bf3803389d8766830e995ddb048130ed4a60
gminardi <gioele.minardi@nais-solutions.it>
2018-02-08 15:37:20 +0100

    Created struct for Point

--- Structure/main.cpp
+++ Structure/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
-
+struct Point {
+    int x;
+    int y;
+};
 void Drawline(int x1, int y1, int x2, int y2) {
 
+}
+void Drawline(Point start, Point end){
+    std::cout << start.x << std::endl;
 }
 
 int main() {
