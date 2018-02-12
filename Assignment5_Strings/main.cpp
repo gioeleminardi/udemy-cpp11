057b3333977952bd903ca8df883a43ca3c8235cf
gminardi <gioele.minardi@nais-solutions.it>
2018-02-12 14:40:15 +0100

    Find Sensitive and Insensitive

---
+++ Assignment5_Strings/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+enum class Case {
+    SENSITIVE, INSENSITIVE
+};
+
+std::string ToLower(const std::string &str) {
+    std::string upperStr{};
+    for (int i = 0; i < str.length(); ++i) {
+        upperStr += static_cast<char>(tolower(str[i]));
+    }
+    return upperStr;
+}
+
+size_t Find(
+        const std::string &source,         //Source string to be searched
+        const std::string &search_string,  //The string to search for
+        Case searchCase = Case::INSENSITIVE,//Choose case sensitive/insensitive search
+        size_t offset = 0) {                //Start the search from this offset
+
+    /*
+    return position of the first character
+    of the substring, else std::string::npos
+    */
+    size_t pos{std::string::npos};
+
+    switch (searchCase) {
+        case Case::INSENSITIVE: {
+            std::string lowerSourceStr{ToLower(source)};
+            pos = lowerSourceStr.find(ToLower(search_string), offset);
+            break;
+        }
+        case Case::SENSITIVE:
+            pos = source.find(search_string, offset);
+            break;
+    }
+    return pos;
+}
+
+int main() {
+    auto pos = Find("ciao", "AO", Case::INSENSITIVE);
+    if (pos != std::string::npos) {
+        std::cout << pos << std::endl;
+    } else {
+        std::cout << "Not found!" << std::endl;
+    }
+    return 0;
+}
