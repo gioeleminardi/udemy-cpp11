b906e9ca4904a402a08fe7aefe68010860f04ad7
gioeleminardi <gioelem3@gmail.com>
2018-02-13 23:54:34 +0100

    throws runtime ex

--- ExceptionHandling/main.cpp
+++ ExceptionHandling/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+void ProcessRecords(int count) {
+    auto *pArray = (int *) malloc(count * sizeof(int));
+    if (pArray == nullptr) {
+        throw std::runtime_error("Failed to allocate memory");
+    }
+    for (int i = 0; i < count; ++i) {
+        pArray[i] = i;
+    }
+    free(pArray);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    try {
+        ProcessRecords(std::numeric_limits<int>::max() * 100);
+    } catch (std::runtime_error &ex) {
+        std::cout << ex.what() << std::endl;
+    }
     return 0;
 }
