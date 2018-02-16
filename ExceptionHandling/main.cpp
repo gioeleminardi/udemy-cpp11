3c4865663d0980595e129d21758e329028d48a7b
gioeleminardi <gioelem3@gmail.com>
2018-02-16 11:45:17 +0100

    nested exceptions

--- ExceptionHandling/main.cpp
+++ ExceptionHandling/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <random>
 
 class Test {
 public:
@@ -15,21 +16,43 @@ public:
 void ProcessRecords(int count) {
     std::unique_ptr<Test> t(new Test);
     if (count < 10) {
-//        throw std::out_of_range("Count should be greater than 10");
+        throw std::out_of_range("Count should be greater than 10");
     }
     std::vector<int> p;
     p.reserve(count);
-    std::vector<int> pArray;
-    pArray.reserve(count);
+    std::vector<int> records;
+    records.reserve(count);
+
+    std::default_random_engine eng;
+    std::bernoulli_distribution dist;
+    int errors{};
     for (int i = 0; i < count; ++i) {
-        pArray.push_back(i);
+        records.push_back(i);
+    }
+
+    for (int i = 0; i < count; ++i) {
+        try {
+            std::cout << "Processing record #: " << i << " ";
+            if (!dist(eng)) {
+                ++errors;
+                throw std::runtime_error("Could not process the record");
+            }
+            std::cout << std::endl;
+        } catch (std::runtime_error &ex) {
+            std::cout << "[ERROR " << ex.what() << "]" << std::endl;
+            if (errors > 4) {
+                std::runtime_error err("Too many errors. Abort.");
+                ex = err;
+                throw;
+            }
+        }
     }
 }
 
 int main() {
     try {
-        ProcessRecords(std::numeric_limits<int>::max() * 100);
-//        ProcessRecords(2);
+//        ProcessRecords(std::numeric_limits<int>::max() * 100);
+        ProcessRecords(22);
     } catch (std::runtime_error &ex) {
         std::cout << ex.what() << std::endl;
     } catch (std::out_of_range &ex) {
