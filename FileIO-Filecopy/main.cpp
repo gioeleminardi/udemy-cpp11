18b648b5ffbe1cac201f7b753c8c96cfbbae5634
gioeleminardi <gioelem3@gmail.com>
2018-02-17 16:10:44 +0100

    File IO Copy utility

---
+++ FileIO-Filecopy/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <experimental/filesystem>
+
+
+int main() {
+    using namespace std::experimental::filesystem;
+    path source(current_path());
+    source /= "Source.cpp";
+    path dest(current_path());
+    dest /= "Copy.cpp";
+    std::ifstream input{source};
+    if (!input) {
+        std::cout << "Source file not found" << std::endl;
+        return -1;
+    }
+    std::ofstream output{dest};
+
+    std::string line;
+    while (!std::getline(input, line).eof()) {
+        output << line << std::endl;
+    }
+    input.close();
+    output.close();
+
+    return 0;
+}
