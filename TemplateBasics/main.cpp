09306bbe53d52dc0bb8ca17bb384a61f43d3565f
gioeleminardi <gioelem3@gmail.com>
2018-02-18 16:29:12 +0100

    Templates

--- TemplateBasics/main.cpp
+++ TemplateBasics/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <cstring>
+
+//Primary template
+template<typename T>
+T Max(T x, T y) {
+    std::cout << typeid(T).name() << std::endl;
+    return x > y ? x : y;
+}
+
+//Explicit Instantiation
+template char Max(char x, char y);
+
+//Explicit Specialization
+template<>
+const char *Max<const char *>(const char *x, const char *y) {
+    std::cout << "Max(char*, char*)" << std::endl;
+    return strcmp(x, y) > 0 ? x : y;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//    auto num = Max(3, 5);
+//    auto num2 = Max('a', 'b');
+//    int (*pFn)(int,int) = Max;
+//    std::cout << num << std::endl;
+//    std::cout << num2 << std::endl;
+
+    const char *b{"B"};
+    const char *a{"A"};
+    auto s = Max(a, b);
+    std::cout << s << std::endl;
     return 0;
 }
