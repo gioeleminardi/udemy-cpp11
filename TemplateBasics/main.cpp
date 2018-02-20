2e7fdac58c31bdb20e4a003dc0ef5c731f54aaf0
gioeleminardi <gioelem3@gmail.com>
2018-02-20 07:18:56 +0100

    Non-Type Parameters

--- TemplateBasics/main.cpp
+++ TemplateBasics/main.cpp
@@ -18,6 +18,32 @@ const char *Max<const char *>(const char *x, const char *y) {
     return strcmp(x, y) > 0 ? x : y;
 }
 
+
+template<int size>
+void Print() {
+    char buffer[size];
+    std::cout << size << std::endl;
+}
+
+//template<typename T>
+//T Sum(T *pArr, int size) {
+//    T sum{};
+//    for (int i = 0; i < size; ++i) {
+//        sum += pArr[i];
+//    }
+//    return sum;
+//}
+
+template<typename T, int size>
+T Sum(T (&pArr)[size]) {
+    T sum{};
+    for (int i = 0; i < size; ++i) {
+        sum += pArr[i];
+    }
+    return sum;
+}
+
+
 int main() {
 //    auto num = Max(3, 5);
 //    auto num2 = Max('a', 'b');
@@ -25,9 +51,15 @@ int main() {
 //    std::cout << num << std::endl;
 //    std::cout << num2 << std::endl;
 
-    const char *b{"B"};
-    const char *a{"A"};
-    auto s = Max(a, b);
-    std::cout << s << std::endl;
+//    const char *b{"B"};
+//    const char *a{"A"};
+//    auto s = Max(a, b);
+//    std::cout << s << std::endl;
+//    Print<3>();
+    int arr[]{3, 1, 9, 7, 4};
+//    int (&ref)[5] = arr;
+    auto it = std::begin(arr);
+    int sum = Sum(arr);
+    std::cout << sum << std::endl;
     return 0;
 }
